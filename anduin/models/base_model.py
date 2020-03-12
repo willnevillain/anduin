be4048d5bfb212e6101b60cabc8e598ee4ff9277
Will Neville <william@neville.com>
2020-03-12 01:54:21 -0400

    Finally, the recursive to_dict method I always wanted <3

--- anduin/models/base_model.py
+++ anduin/models/base_model.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm.collections import InstrumentedList
 
 from anduin.database import db
 
@@ -47,3 +48,21 @@ class BaseModel(db.Model):
         """
         db.session.delete(self)
         db.session.commit()
+
+    def to_dict(self, recurse_relationships=True):
+        """
+        Returns dictionary representation of object. Relationships are recursed and converted to dicts if specified.
+        :param recurse_relationships: whether or not to recurse and convert relationship objects to dicts.
+        :type recurse_relationships: Boolean
+        """
+        model_dict = {}
+        for column in self.__table__.columns:
+            model_dict[column.name] = getattr(self, column.name)
+        if recurse_relationships:
+            for rel in self.__mapper__.relationships.keys():
+                rel_result = getattr(self, rel)
+                if type(rel_result) == InstrumentedList:  # If it's a many relationship, we get a list
+                    model_dict[rel] = [model.to_dict(recurse_relationships=False) for model in rel_result]
+                else:  # Else we get a single model
+                    model_dict[rel] = rel_result.to_dict()
+        return model_dict
